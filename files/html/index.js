8520aaa64536af5936570de4a4eb244e085413fd
BrandonChee <brandonch9029@gmail.com>
2022-03-07 21:39:02 +0000

    Wrote a bunch of functions
    Functions for populating period list and change of period

--- files/html/index.js
+++ files/html/index.js
@@ -9,10 +9,27 @@ getPeriods()
 
 function createPeriodList (periodList) {
     document.getElementById('periodDrop').innerHTML = `
-    <select>
+    <select onchange='loadPeriodName(this.value)'>
         ${Object.keys(periodList).map(function (period) {
             return `<option>${period}</option>`
         }).join('')}
     </select>
     `
+}
+
+async function loadPeriodName (period) {
+    document.getElementById('periodName').innerHTML = `
+    ${period}
+    `
+    let infoString = await getPeriodInfo(period)
+    document.getElementById('periodInfo').innerHTML = `
+    ${infoString}
+    `
+}
+
+async function getPeriodInfo (period) {
+    const response = await fetch('http://localhost:3000/static/json/periods.json')
+    const data = await response.json()
+    infoString = data[period][0].keyinfo
+    return infoString
 }

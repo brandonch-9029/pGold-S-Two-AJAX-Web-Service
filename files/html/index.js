c17767fd4bde7e49be950b74652d0cf76d93bb91
BrandonChee <brandonch9029@gmail.com>
2022-03-08 02:32:06 +0000

    Starting on functions for artists

--- files/html/index.js
+++ files/html/index.js
@@ -18,18 +18,39 @@ function createPeriodList (periodList) {
 async function loadPeriodName (period) {
     let infoString = await getPeriodInfo(period)
     document.getElementById('periodInfo').innerHTML = `
-    ${infoString}
+    ${description}
+    `
+    document.getElementById('artist1').innerHTML = `
+    <button class="btn btn-danger btn-lg" type="button" onclick='updateArtistInfo("one")'>${artistOne}</button>
+    `
+    document.getElementById('artist2').innerHTML = `
+    <button class="btn btn-primary btn-lg" type="button" onclick='updateArtistInfo("two")'>${artistTwo}</button>
+    `
+    document.getElementById('artist3').innerHTML = `
+    <button class="btn btn-warning btn-lg" type="button" onclick='updateArtistInfo("three")'>${artistThree}</button>
     `
 }
 
 async function getPeriodInfo (period) {
     const response = await fetch('http://localhost:3000/static/json/periods.json')
     const data = await response.json()
-    infoString = data[period][0].keyinfo
+    description = data[period][0].keyinfo
+    artistOne = data[period][0].keyartists[0].name
+    artistTwo = data[period][0].keyartists[1].name
+    artistThree = data[period][0].keyartists[2].name
+    return {description, artistOne, artistTwo, artistThree}
+}
 
-    console.log(data[period][0].keyartists[0])
-
-    return infoString
+function updateArtistInfo (artistIndex) {
+    if (artistIndex == "one") {
+        console.log("clicked on first artist");
+    }
+    if (artistIndex == "two") {
+        console.log("clicked on second artist");
+    }
+    if (artistIndex == "three") {
+        console.log("clicked on third artist");
+    }
 }
 
 getPeriods()
